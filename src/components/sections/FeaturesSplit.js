4aae6eb5314fb9217280d98d50e22f34a29deaa0
Imad Bin Asad <imadbinasad1@gmail.com>
2023-01-23 17:30:26 +0530

    website2

---
+++ src/components/sections/FeaturesSplit.js
@@ -0,0 +1,156 @@
+import React from 'react';
+import classNames from 'classnames';
+import { SectionSplitProps } from '../../utils/SectionProps';
+import SectionHeader from './partials/SectionHeader';
+import Image from '../elements/Image';
+
+const propTypes = {
+  ...SectionSplitProps.types
+}
+
+const defaultProps = {
+  ...SectionSplitProps.defaults
+}
+
+const FeaturesSplit = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  invertMobile,
+  invertDesktop,
+  alignTop,
+  imageFill,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'features-split section',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'features-split-inner section-inner',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  const splitClasses = classNames(
+    'split-wrap',
+    invertMobile && 'invert-mobile',
+    invertDesktop && 'invert-desktop',
+    alignTop && 'align-top'
+  );
+
+  const sectionHeader = {
+    title: 'Why should you join us?',
+    paragraph: 'These are some of the factors that may convince you to join us'
+  };
+
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+      id="benefits"
+    >
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={splitClasses}>
+
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  {/* Lightning fast workflow */}
+                  </div>
+                <h3 className="mt-0 mb-12">
+                  Frequent Meets
+                  </h3>
+                <p className="m-0" style={{textAlign:"justify"}}>
+                  We conduct regular meets. It is hard to solve a major issue single-handedly. A member may be struggling for a solution to a problem only to find that other members can fix the issue. In this case, a team meeting can provide an opportunity for members to share their problems and concerns. 
+                  </p>
+              </div>
+              <div className={
+                classNames(
+                  'split-item-image center-content-mobile reveal-from-bottom',
+                  imageFill && 'split-item-image-fill'
+                )}
+                data-reveal-container=".split-item">
+                <Image
+                  src={require('./../../assets/images/meet.gif')}
+                  alt="Features split 01"
+                  width={528}
+                  height={396} />
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  {/* Lightning fast workflo */}
+                  </div>
+                <h3 className="mt-0 mb-12">
+                  Awsome Mentors
+                  </h3>
+                <p className="m-0" style={{textAlign:"justify"}}>
+                 We have awsome mentors to motivate us. All of them are placed in big companies. Mentorship can provide numerous benefits for mentors and their mentees. Developing this relationship can help both to learn new things, build networks and grow as professionals. Mentors encourage and enable another person's professional or personal development. A mentor can help focus their efforts by setting goals and giving feedback. Mentors can provide specific insights and information that enable the mentee's success. A mentor can help their mentee set personal or professional development goals. 
+                  </p>
+              </div>
+              <div className={
+                classNames(
+                  'split-item-image center-content-mobile reveal-from-bottom',
+                  imageFill && 'split-item-image-fill'
+                )}
+                data-reveal-container=".split-item">
+                <Image
+                  src={require('./../../assets/images/mentors.png')}
+                  alt="Features split 02"
+                  width={528}
+                  height={396} />
+              </div>
+            </div>
+
+            {/* <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Lightning fast workflow
+                  </div>
+                <h3 className="mt-0 mb-12">
+                  Data-driven insights
+                  </h3>
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+              </div>
+              <div className={
+                classNames(
+                  'split-item-image center-content-mobile reveal-from-bottom',
+                  imageFill && 'split-item-image-fill'
+                )}
+                data-reveal-container=".split-item">
+                <Image
+                  src={require('./../../assets/images/features-split-image-03.png')}
+                  alt="Features split 03"
+                  width={528}
+                  height={396} />
+              </div>
+            </div> */}
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+FeaturesSplit.propTypes = propTypes;
+FeaturesSplit.defaultProps = defaultProps;
+
+export default FeaturesSplit;
---
+++ src/components/sections/Testimonial.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import classNames from 'classnames';
+import { SectionTilesProps } from '../../utils/SectionProps';
+import SectionHeader from './partials/SectionHeader';
+
+const propTypes = {
+  ...SectionTilesProps.types
+}
+
+const defaultProps = {
+  ...SectionTilesProps.defaults
+}
+
+const Testimonial = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  pushLeft,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'testimonial section',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'testimonial-inner section-inner',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  const tilesClasses = classNames(
+    'tiles-wrap',
+    pushLeft && 'push-left'
+  );
+
+  const sectionHeader = {
+    title: 'Our Team',
+    paragraph: 'We introduce you to our glorious team'
+  };
+
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+      id="team"
+    >
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={tilesClasses}>
+
+            <div className="tiles-item reveal-from-right" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="testimonial-item-content">
+                  <img src={require("../../assets/images/Team/maaz.jpg") }/>
+                </div>
+                <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
+                  <span className="testimonial-item-name text-color-high">Maaz Bin Asad</span>
+                  <span className="text-color-low"> / </span>
+                  <span className="testimonial-item-link">
+                    <a href="#0">Mentor</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="testimonial-item-content">
+                <img src={require("../../assets/images/Team/apoorve.jpg") }/>
+                </div>
+                <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
+                  <span className="testimonial-item-name text-color-high">Apoorve Goel</span>
+                  <span className="text-color-low"> / </span>
+                  <span className="testimonial-item-link">
+                    <a href="#0">Mentor</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-left" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="testimonial-item-content">
+                <img src={require("../../assets/images/Team/sanjana.jpg") }/>
+                </div>
+                <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
+                  <span className="testimonial-item-name text-color-high">Sanjana Maheshwari</span>
+                  <span className="text-color-low"> / </span>
+                  <span className="testimonial-item-link">
+                    <a href="#0">Mentor</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            {/* <div className="tiles-item reveal-from-left" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="testimonial-item-content">
+                <img src={require("../../assets/images/Team/sanjana.jpg") }/>
+                </div>
+                <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
+                  <span className="testimonial-item-name text-color-high">Ben Stafford</span>
+                  <span className="text-color-low"> / </span>
+                  <span className="testimonial-item-link">
+                    <a href="#0">AppName</a>
+                  </span>
+                </div>
+              </div>
+            </div> */}
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+Testimonial.propTypes = propTypes;
+Testimonial.defaultProps = defaultProps;
+
+export default Testimonial;
