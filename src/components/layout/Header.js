13bca15271695ae2bb765d2e785d51d44c1b91da
Imad Bin Asad <imadbinasad1@gmail.com>
2023-02-01 00:03:33 +0530

    form

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -129,7 +129,7 @@ const Header = ({
                       className="list-reset header-nav-right"
                     >
                       <li>
-                        <Link to="/sign-up" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Sign up</Link>
+                        {/* <Link to="/sign-up" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Sign up</Link> */}
                       </li>
                     </ul>}
                 </div>
